ccd138cd186a8e5e3e997bdc8f1a92401b15fe47
austin-x-km <karurimm@gmail.com>
2023-02-19 11:33:21 -0800

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-/**
- * Main: A program that prints a line with put function
- * Return: 0(success)
+/*
+ * This program demonstrates the use of the puts() function to print a string to
+ * the console.
  */
 int main(void)
 {
+/*
+ *The main() function is the entry point of the program. It calls the puts()
+ * function to print the string "Programming is like building a multilingual
+ * puzzle" to the console, followed by a newline character. Finally, it returns
+ * 0 to indicate successful program termination.
+ */	
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
