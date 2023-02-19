20e080c3a9ff6c190058371e9b9fa7a442d152ad
austin-x-km <karurimm@gmail.com>
2023-02-19 14:50:20 -0800

    update 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,27 +4,26 @@
 int main(void)
 {
 int n;
-
-    /* Initialize the random number generator with the current time */
+/* Initialize the random number generator with the current time */
 srand(time(0));
-
-    /* Generate a random number between -RAND_MAX and RAND_MAX and store it in n */
+/* Generate a random number between -RAND_MAX 
+ * and RAND_MAX and store it in n 
+ * */
 n = rand() - RAND_MAX / 2;
 
-    /* Print the number and whether it is positive, negative, or zero */
-    
+/* Print the number and whether it is positive, negative, or zero */
 if (n > 0)
- {
+{
 printf("%d is positive\n" , n);
- } 
+} 
 else if (n < 0) 
-    {
+{
 printf("%d is negative\n", n);
-    }
+}
 else 
-    {
+{
 printf("%d is zero", n);
-    }
+}
 printf("\n");
 return (0);
 }
