0d00c1a557f59a058967caea380d034c816cb52f
austin-x-km <karurimm@gmail.com>
2023-02-19 11:59:51 -0800

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,12 +7,11 @@
 
 int main(void)
 {
-    /*
-     * The main() function is the entry point of the program. It calls the printf()
-     * function to print the string "Art is a language, and I am fluent." to the
-     * console, followed by a newline character. Finally, it returns 0 to indicate
-     * successful program termination.
-     */
+/*
+ * main() function is the entry point of the program.
+ * Finally, it returns 0 to indicate
+ * successful program termination.
+ */
 printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
