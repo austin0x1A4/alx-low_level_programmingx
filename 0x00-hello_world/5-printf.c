de639315b5398ee683db047ca5b632d17861d6f5
austin-x-km <karurimm@gmail.com>
2023-02-19 11:45:57 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * This program demonstrates the use of the printf() function to print a string
+ * with proper grammar to the console.
+ */
+
+int main()
+{
+    /*
+     * The main() function is the entry point of the program. It calls the printf()
+     * function to print the string "Art is a language, and I am fluent." to the
+     * console, followed by a newline character. Finally, it returns 0 to indicate
+     * successful program termination.
+     */
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+return 0;
+}
