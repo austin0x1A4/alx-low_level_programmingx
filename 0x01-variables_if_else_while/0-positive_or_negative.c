cc0894a4576978faabdd4b2fad0586f2eca477a6
austin-x-km <karurimm@gmail.com>
2023-02-20 10:12:47 -0800

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ n = rand() - RAND_MAX / 2;
 /* Print the number and whether it is positive negative or zero */
 if (n > 0)
 {
-printf("%d is positive\n", n);
+printf("%d is positive", n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n", n);
+printf("%d is zero", n);
 }
 else
 {
