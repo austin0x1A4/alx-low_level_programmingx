6ae1a6fe4698db526dc87ba1816b8614e1a8b09f
austin-x-km <karurimm@gmail.com>
2023-02-20 11:17:04 -0800

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* any headers go here */
+
+/**
+ * main function - entry point
+ * return: Always 0(success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+
+putchar('\n');
+
+return (0);
+}
+
