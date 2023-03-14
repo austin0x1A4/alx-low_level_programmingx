26b3082c5358b42a6ded9855a080320e4db4094d
austin-x-km <karurimm@gmail.com>
2023-03-14 13:16:23 -0700

    Update 0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,66 +18,63 @@ char *str_concat(char *s1, char *s2)
 		
 {
 		
-	char *conct;
+char *conct;
 		
-	int i, ci;
+int i, ci;
+				
+if (s1 == NULL)
 		
-
+s1 = "";
 		
-	if (s1 == NULL)
+if (s2 == NULL)
 		
-		s1 = "";
-		
-	if (s2 == NULL)
-		
-		s2 = "";
+s2 = "";
 		
 
 		
-		i = ci = 0;
+i = ci = 0;
 		
-	while (s1[i] != '\0')
+while (s1[i] != '\0')
 		
-		i++;
+i++;
 		
-	while (s2[ci] != '\0')
+while (s2[ci] != '\0')
 		
-		ci++;
+ci++;
 		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
-
-		
-	if (conct == NULL)
-		
-		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
-	}
+conct = malloc(sizeof(char) * (i + ci + 1));
 		
 
 		
-	while (s2[ci] != '\0')
+if (conct == NULL)
 		
-	{
+return (NULL);
 		
-		conct[i] = s2[ci];
+i = ci = 0;
 		
-		i++, ci++;
+while (s1[i] != '\0')
 		
-	}
+{
 		
-	conct[i] = '\0';
+conct[i] = s1[i];
 		
-	return (conct);
+i++;
+		
+}
+		
+
+		
+while (s2[ci] != '\0')
+		
+{
+		
+conct[i] = s2[ci];
+		
+i++, ci++;
+}
+		
+conct[i] = '\0';
+		
+return (conct);
 		
 }
